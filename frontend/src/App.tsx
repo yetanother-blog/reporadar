289d4d67f907ea0f9a470d4b0bb0afe8b93f0d9c
Henrik Fricke <henrikfricke@web.de>
2020-04-05 19:52:02 +0200

    chore: add material ui

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,12 +1,16 @@
 import React from "react";
 import { ApolloProvider } from "@apollo/react-hooks";
+import { CssBaseline } from '@material-ui/core';
 import { client } from "./utils/apollo";
 import { RepoList } from "./containers/RepoList/RepoList";
 
 export const App: React.FC = () => {
   return (
     <ApolloProvider client={client}>
-      <RepoList />
+      <>
+        <CssBaseline />
+        <RepoList />
+      </>
     </ApolloProvider>
   );
 };
