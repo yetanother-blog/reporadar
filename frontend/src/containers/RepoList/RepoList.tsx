828982e281287d346e580fe8eb21c1ea30218fa2
Henrik Fricke <henrikfricke@web.de>
2020-04-10 19:50:15 +0200

    feat(fe): timeline

--- frontend/src/containers/RepoList/RepoList.tsx
+++ frontend/src/containers/RepoList/RepoList.tsx
@@ -2,13 +2,14 @@ import React from "react";
 import { useRepoListQuery } from "../../generated/graphql";
 import { RepoCard } from "../../ui/RepoCard/RepoCard";
 import { RepoCardList } from "../../ui/RepoCardList/RepoCardList";
+import { sortByDay } from "../../utils/sortByDay";
 
 export const RepoList: React.FC = () => {
   const { loading, data } = useRepoListQuery();
 
   if (loading) {
     return (
-      <RepoCardList>
+      <RepoCardList time="2020-01-01">
         <RepoCard name="" description="" url="" stars={0} loading={true} />
         <RepoCard name="" description="" url="" stars={0} loading={true} />
         <RepoCard name="" description="" url="" stars={0} loading={true} />
@@ -16,19 +17,27 @@ export const RepoList: React.FC = () => {
     );
   }
 
-  if (!data) return null;
+  if (!data || !data.allRepos) {
+    return null;
+  }
+
+  const days = sortByDay(data.allRepos.repos, "indexedAt");
 
   return (
-    <RepoCardList>
-      {data.allRepos.repos.map((repo) => (
-        <RepoCard
-          key={repo.id}
-          name={repo.id}
-          description={repo.description}
-          url={repo.url}
-          stars={repo.numberOfStars}
-        />
+    <>
+      {days.map(day => (
+        <RepoCardList time={day.time} key={day.time}>
+          {day.entities.map(repo => (
+            <RepoCard
+              key={repo.id}
+              name={repo.id}
+              description={repo.description}
+              url={repo.url}
+              stars={repo.numberOfStars}
+            />
+          ))}
+        </RepoCardList>
       ))}
-    </RepoCardList>
+    </>
   );
 };
--- frontend/src/ui/RepoCardList/RepoCardList.tsx
+++ frontend/src/ui/RepoCardList/RepoCardList.tsx
@@ -1,12 +1,25 @@
 import React from "react";
-import { Grid } from "@material-ui/core";
+import { Grid, Box, Typography } from "@material-ui/core";
+import moment from "moment";
 
-export const RepoCardList: React.FC = ({ children }) => {
+export interface RepoCardListProps {
+  time: string;
+}
+
+export const RepoCardList: React.FC<RepoCardListProps> = ({
+  children,
+  time
+}) => {
   return (
-    <Grid container direction="column" spacing={3}>
-      {React.Children.map(children, (child) => (
-        <Grid item>{child}</Grid>
-      ))}
-    </Grid>
+    <Box marginBottom={4}>
+      <Typography variant="h4" gutterBottom>
+        {moment(time, "YYYYMMDD").fromNow()}
+      </Typography>
+      <Grid container direction="column" spacing={3}>
+        {React.Children.map(children, child => (
+          <Grid item>{child}</Grid>
+        ))}
+      </Grid>
+    </Box>
   );
 };
