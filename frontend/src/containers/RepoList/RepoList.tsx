0e94088d8e3afa26b137e1f85939698069a7947f
Henrik Fricke <henrikfricke@web.de>
2020-04-11 00:40:46 +0200

    feat(fe): infinite scrolling for repo list

--- frontend/src/containers/RepoList/RepoList.tsx
+++ frontend/src/containers/RepoList/RepoList.tsx
@@ -1,30 +1,61 @@
 import React from "react";
+import InfiniteScroll from "react-infinite-scroller";
 import { useRepoListQuery } from "../../generated/graphql";
 import { RepoCard } from "../../ui/RepoCard/RepoCard";
 import { RepoCardList } from "../../ui/RepoCardList/RepoCardList";
 import { sortByDay } from "../../utils/sortByDay";
 
+const LIMIT = 50;
+
+export const RepoListSkeleton: React.FC = () => (
+  <RepoCardList time="2020-01-01" isLoading>
+    <RepoCard name="" description="" url="" stars={0} loading />
+    <RepoCard name="" description="" url="" stars={0} loading />
+    <RepoCard name="" description="" url="" stars={0} loading />
+  </RepoCardList>
+);
+
 export const RepoList: React.FC = () => {
-  const { loading, data } = useRepoListQuery();
+  const { loading, data, fetchMore } = useRepoListQuery({
+    variables: { limit: LIMIT }
+  });
 
   if (loading) {
-    return (
-      <RepoCardList time="2020-01-01" isLoading>
-        <RepoCard name="" description="" url="" stars={0} loading />
-        <RepoCard name="" description="" url="" stars={0} loading />
-        <RepoCard name="" description="" url="" stars={0} loading />
-      </RepoCardList>
-    );
+    return <RepoListSkeleton />;
   }
 
   if (!data || !data.allRepos) {
     return null;
   }
 
+  const loadMore = () => {
+    fetchMore({
+      variables: { nextToken: data.allRepos.nextToken },
+      updateQuery(prev, { fetchMoreResult }) {
+        return fetchMoreResult
+          ? {
+              allRepos: {
+                ...prev.allRepos,
+                repos: [
+                  ...prev.allRepos.repos,
+                  ...fetchMoreResult.allRepos.repos
+                ],
+                nextToken: fetchMoreResult.allRepos.nextToken
+              }
+            }
+          : prev;
+      }
+    });
+  };
+
   const days = sortByDay(data.allRepos.repos, "indexedAt");
 
   return (
-    <>
+    <InfiniteScroll
+      loadMore={loadMore}
+      hasMore={!!data.allRepos.nextToken}
+      loader={<RepoListSkeleton key="repo-list-skeleton" />}
+    >
       {days.map(day => (
         <RepoCardList time={day.time} key={day.time}>
           {day.entities.map(repo => (
@@ -38,6 +69,6 @@ export const RepoList: React.FC = () => {
           ))}
         </RepoCardList>
       ))}
-    </>
+    </InfiniteScroll>
   );
 };
